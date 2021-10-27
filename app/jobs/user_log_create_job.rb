9cabc8bbec5cd03b00b64724dd516580d70cd46f
Kris Litman <kris.d.litman@gmail.com>
2021-10-26 22:26:20 -0400

    Added callback method to userlogcreatejob class

--- app/jobs/user_log_create_job.rb
+++ app/jobs/user_log_create_job.rb
@@ -1,6 +1,10 @@
 class UserLogCreateJob < ApplicationJob
   queue_as :default
 
+  after_perform do |job|
+    Rails.logger.info "#{Time.now}: Job completed. #{job.inspect}"
+  end
+
   def perform(user_id)
     UserLog.create(
       user_id: user_id,
